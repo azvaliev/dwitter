6e36098e6c2074ef0e0bb33efe4ad8911b57bc70
azat valiev <azvaliev@gmail.com>
2022-12-18 09:44:19 -0600

    added fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,27 @@ import { type AppType } from "next/dist/shared/lib/utils";
 
 import "src/styles/globals.css";
 
+import { Inter, Source_Sans_Pro } from '@next/font/google'
+
+const inter = Inter({
+  variable: '--font-inter',
+});
+
+const sourceSansPro = Source_Sans_Pro({
+  variable: '--font-source-sans-pro',
+  weight: ["200", "300", "400"],
+});
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        * { font-family: ${sourceSansPro.style.fontFamily}; }
+        :is(h1, h2, h3, h4, h5, h6), :is(h1, h2, h3, h4, h5, h6) * { font-family: ${inter.style.fontFamily}; }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        serif: ['var(--font-inter)'],
+        sans: ['var(--font-source-sans-pro)']
+      },
+    },
   },
   plugins: [],
 };
